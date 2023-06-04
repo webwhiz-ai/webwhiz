41a9aeb00780be7f863a6572c2ed86e0dd433983
Kurien Zacharia <kurienzach@gmail.com>
2023-06-04 07:45:21 +0530

    [Change] Change env vars for Google Auth

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
-export const GOOGLE_AUTH_ID = process.env.GOOGLE_AUTH_ID;
+export const GOOGLE_AUTH_ID = process.env.REACT_APP_GOOGLE_AUTH_ID;
 export const baseURL = process.env.REACT_APP_BASE_URL;
