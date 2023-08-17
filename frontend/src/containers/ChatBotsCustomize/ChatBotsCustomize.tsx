272dc593fd3d8dc1d656a9f4bfe8f694b5410dd3
Hrithik T <43098636+hrithikt@users.noreply.github.com>
2023-08-18 01:19:02 +0530

    dynamically change placeholder in preview

--- frontend/src/containers/ChatBotsCustomize/ChatBotsCustomize.tsx
+++ frontend/src/containers/ChatBotsCustomize/ChatBotsCustomize.tsx
@@ -527,7 +527,7 @@ export const ChatBotsCustomize = ({
 												</div>
 												<Box id="chat-form">
 													<div className="chat-input-wrap">
-														<textarea value={message} onChange={handleTextAreaChange} rows="1" className="chat-input textarea js-auto-size" id="chat-input" placeholder="Type your message" ></textarea>
+														<textarea value={message} onChange={handleTextAreaChange} rows="1" className="chat-input textarea js-auto-size" id="chat-input" placeholder={values.chatInputPlaceholderText} ></textarea>
 														<button className="chat-submit-btn"><svg width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
 															<path fillRule="evenodd" clipRule="evenodd" d="M4.394 14.7L13.75 9.3c1-.577 1-2.02 0-2.598L4.394 1.299a1.5 1.5 0 00-2.25 1.3v3.438l4.059 1.088c.494.132.494.833 0 .966l-4.06 1.087v4.224a1.5 1.5 0 002.25 1.299z" style={{ fill: values.backgroundColor }}></path>
 														</svg></button>
