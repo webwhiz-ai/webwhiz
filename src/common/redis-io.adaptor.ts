482e260a77e51d9cf37ca5e78e688fc71eccce25
Renjith K R <renjithkr123@gmail.com>
2024-01-04 10:47:11 +0530

    Added redis adaptor for socket io

---
+++ src/common/redis-io.adaptor.ts
@@ -0,0 +1,32 @@
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import { ServerOptions } from 'socket.io';
+import { createAdapter } from '@socket.io/redis-adapter';
+import { AppConfigService } from './config/appConfig.service';
+import { createClient } from 'redis';
+
+export class RedisIoAdapter extends IoAdapter {
+  private adapterConstructor: ReturnType<typeof createAdapter>;
+  private appConfig: AppConfigService;
+
+  // constructor(appConfig: AppConfigService) {
+  //   super();
+  //   this.appConfig = appConfig;
+  // }
+
+  async connectToRedis(): Promise<void> {
+    //let redisUrl = this.appConfig.get('redisUrl');
+
+    const pubClient = createClient({ url: `redis://localhost:6379` });
+    const subClient = pubClient.duplicate();
+
+    // await Promise.all([pubClient.connect(), subClient.connect()]);
+
+    this.adapterConstructor = createAdapter(pubClient, subClient);
+  }
+
+  createIOServer(port: number, options?: ServerOptions): any {
+    const server = super.createIOServer(port, options);
+    server.adapter(this.adapterConstructor);
+    return server;
+  }
+}
