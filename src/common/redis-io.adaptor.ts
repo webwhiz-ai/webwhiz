e146b1c513cb2089ba207d0140c921f3c5a3be5f
Sachin Neravath <sachi77n@gmail.com>
2024-01-08 16:52:20 +0530

    import app config in redis adaptor

--- src/common/redis-io.adaptor.ts
+++ src/common/redis-io.adaptor.ts
@@ -3,11 +3,18 @@ import { ServerOptions } from 'socket.io';
 import { createAdapter } from '@socket.io/redis-adapter';
 import { AppConfigService } from './config/appConfig.service';
 import { createClient } from 'redis';
+import { INestApplicationContext } from '@nestjs/common';
+import { REDIS } from './redis/redis.module';
 
 export class RedisIoAdapter extends IoAdapter {
   private adapterConstructor: ReturnType<typeof createAdapter>;
   private appConfig: AppConfigService;
 
+  constructor(private app: INestApplicationContext) {
+    super();
+    this.appConfig = app.get(AppConfigService);
+    console.log('appConfig', this.appConfig);
+  }
   // constructor(appConfig: AppConfigService) {
   //   super();
   //   this.appConfig = appConfig;
@@ -15,6 +22,13 @@ export class RedisIoAdapter extends IoAdapter {
 
   async connectToRedis(): Promise<void> {
     //let redisUrl = this.appConfig.get('redisUrl');
+    // console.log('Connecting to redis', process.env, app);
+
+    const redisUrl = this.appConfig.get('redisUrl');
+
+    const redis = this.app.get(REDIS);
+
+    console.log('redis', redis);
 
     const pubClient = createClient({ url: `redis://localhost:6379` });
     const subClient = pubClient.duplicate();
