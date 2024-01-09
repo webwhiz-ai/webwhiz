61d91fef0eb30b356fbb17aaa251a76bb80c8805
Hrithik T <43098636+hrithikt@users.noreply.github.com>
2024-01-09 11:14:02 +0530

    [api-key] added controller and service

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,15 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { MongoModule } from '../common/mongo/mongo.module';
 import { SubscriptionModule } from '../subscription/subscription.module';
+import { ApikeyController } from './apikey/apikey.controller';
+import { ApikeyService } from './apikey/apikey.service';
 import { UsersController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   imports: [MongoModule, forwardRef(() => SubscriptionModule)],
-  controllers: [UsersController],
-  providers: [UserService],
+  controllers: [UsersController, ApikeyController],
+  providers: [UserService, ApikeyService],
   exports: [UserService],
 })
 export class UserModule {}
