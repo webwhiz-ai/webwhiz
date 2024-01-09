3e437d04cfb7718bc9950745544b21be579038b7
Renjith K R <renjithkr123@gmail.com>
2024-01-09 13:12:43 +0530

    Adding env variables

--- src/common/redis-io.adaptor.ts
+++ src/common/redis-io.adaptor.ts
@@ -1,39 +1,22 @@
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { ServerOptions } from 'socket.io';
 import { createAdapter } from '@socket.io/redis-adapter';
-import { AppConfigService } from './config/appConfig.service';
 import { createClient } from 'redis';
-import { INestApplicationContext } from '@nestjs/common';
-import { REDIS } from './redis/redis.module';
 
 export class RedisIoAdapter extends IoAdapter {
   private adapterConstructor: ReturnType<typeof createAdapter>;
-  private appConfig: AppConfigService;
-
-  constructor(private app: INestApplicationContext) {
-    super();
-    this.appConfig = app.get(AppConfigService);
-    console.log('appConfig', this.appConfig);
-  }
-  // constructor(appConfig: AppConfigService) {
-  //   super();
-  //   this.appConfig = appConfig;
-  // }
 
   async connectToRedis(): Promise<void> {
-    //let redisUrl = this.appConfig.get('redisUrl');
-    // console.log('Connecting to redis', process.env, app);
+    let redisUrl;
+    if (process.env['REDIS_URL']) {
+      redisUrl = process.env['REDIS_URL'];
+    } else {
+      redisUrl = `redis://${process.env['REDIS_HOST']}:${process.env['REDIS_PORT']}`;
+    }
+    console.log('Connecting to redis', redisUrl);
 
-    const redisUrl = this.appConfig.get('redisUrl');
-
-    const redis = this.app.get(REDIS);
-
-    console.log('redis', redis);
-
-    const pubClient = createClient({ url: `redis://localhost:6379` });
+    const pubClient = createClient({ url: redisUrl });
     const subClient = pubClient.duplicate();
-
-    // await Promise.all([pubClient.connect(), subClient.connect()]);
 
     this.adapterConstructor = createAdapter(pubClient, subClient);
   }
