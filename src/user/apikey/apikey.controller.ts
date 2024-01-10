b2adade7e91323814d299a4829814ab32fcb2f4e
Hrithik T <43098636+hrithikt@users.noreply.github.com>
2024-01-10 12:23:05 +0530

    [api-key] implemented create apikey endpoint

--- src/user/apikey/apikey.controller.ts
+++ src/user/apikey/apikey.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Req, Delete, Get } from '@nestjs/common';
+import { Controller, Post, Req, Delete, Get, HttpCode } from '@nestjs/common';
 import { RequestWithUser } from '../../common/@types/nest.types';
 import { ApikeyService } from './apikey.service';
 
@@ -12,7 +12,13 @@ export class ApikeyController {
     return this.apikeyService.getAllApiKeys(user);
   }
 
+  /**
+   * Creates a new API key for the user.
+   * @param req The request object containing the user information.
+   * @returns The created API key.
+   */
   @Post()
+  @HttpCode(201)
   createApiKey(@Req() req: RequestWithUser) {
     const { user } = req;
     return this.apikeyService.createApiKey(user);
