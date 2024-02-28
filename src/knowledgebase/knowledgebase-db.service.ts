a97edd494b519227a7efc5d19d61c50e15ff0d26
Hrithik T <43098636+hrithikt@users.noreply.github.com>
2024-02-28 15:48:16 +0530

    replace save with insert while inserting into postgres

--- src/knowledgebase/knowledgebase-db.service.ts
+++ src/knowledgebase/knowledgebase-db.service.ts
@@ -478,8 +478,7 @@ export class KnowledgebaseDbService {
   }
 
   async insertEmbeddingsToPg(data: KbEmbeddings) {
-    // await this.kbEmbeddingCollection.insertOne(data);
-    await this.usersRepository.save(data);
+    await this.usersRepository.insert(data);
   }
 
   /**
