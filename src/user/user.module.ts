a6bc1f648b2bf48d25bd58c1ff014ad5c292b436
Hrithik T <43098636+hrithikt@users.noreply.github.com>
2024-02-28 15:55:12 +0530

    add typeorm import in usermodule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,18 @@
 import { forwardRef, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { KbEmbeddings } from '../common/entity/kbEmbeddings.entity';
 import { MongoModule } from '../common/mongo/mongo.module';
 import { SubscriptionModule } from '../subscription/subscription.module';
 import { UsersController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [MongoModule, forwardRef(() => SubscriptionModule)],
+  // TODO: Check why TypeOrmModule.forFeature is required here
+  imports: [
+    MongoModule,
+    forwardRef(() => SubscriptionModule),
+    TypeOrmModule.forFeature([KbEmbeddings]),
+  ],
   controllers: [UsersController],
   providers: [UserService],
   exports: [UserService],
