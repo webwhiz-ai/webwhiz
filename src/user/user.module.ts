9e1184a95098ad21b9a5d31576d85363909aea12
Hrithik T <43098636+hrithikt@users.noreply.github.com>
2024-03-24 19:03:18 +0530

    [fix] controllers in user.module giving request as undefined

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,6 @@
 import { forwardRef, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { KbEmbeddingsPg } from '../common/entity/kbEmbeddings.entity';
 import { MongoModule } from '../common/mongo/mongo.module';
 import { SubscriptionModule } from '../subscription/subscription.module';
 import { ApikeyController } from './apikey/apikey.controller';
@@ -7,7 +9,12 @@ import { UsersController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [MongoModule, forwardRef(() => SubscriptionModule)],
+  imports: [
+    MongoModule,
+    forwardRef(() => SubscriptionModule),
+    // TODO: Understand why TypeOrmModule.forFeature is needed here. user.service was failing without it.
+    TypeOrmModule.forFeature([KbEmbeddingsPg]),
+  ],
   controllers: [UsersController, ApikeyController],
   providers: [UserService, ApikeyService],
   exports: [UserService],
