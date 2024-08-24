e26f5074929904a9bc7a10f1327d7513eff9693a
hrithikt <hrithikt1999@gmail.com>
2024-08-24 20:20:08 +0530

    integrated feature-flags to app-config

--- src/common/config/configuration.ts
+++ src/common/config/configuration.ts
@@ -1,5 +1,8 @@
 import { Subscription } from '../../user/user.schema';
 
+export interface FeatureFlags {
+  [key: string]: boolean;
+}
 interface AppConfig {
   nodeEnv: 'development' | 'production' | 'test';
   host: string;
@@ -29,41 +32,55 @@ interface AppConfig {
   postgresUser: string;
   postgresPassword: string;
   postgresDbName: string;
+  featureFlags: FeatureFlags;
 }
 
-const config = (): AppConfig => ({
-  nodeEnv: (process.env.NODE_ENV as any) || 'development',
-  host: process.env.HOST || '127.0.0.1',
-  port: parseInt(process.env.PORT as string, 10) || 8000,
-  secretKey: process.env.SECRET_KEY || 'secret',
-  encryptionKey:
-    process.env.ENC_KEY ||
-    '4b23f673b133b01777259525a4a1c2097fe309048db6593462312d5c35679ced',
-  mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017',
-  mongoDbName: process.env.MONGO_DBNAME || 'sitemine',
-  redisHost: process.env.REDIS_HOST || 'localhost',
-  redisPort: parseInt(process.env.REDIS_PORT as string, 10) || 6379,
-  redisUrl: process.env.REDIS_URL,
-  openaiKey: process.env.OPENAI_KEY || '',
-  openaiKey2: process.env.OPENAI_KEY_2 || '',
-  googleClientId: process.env.GOOGLE_CLIENT_ID || '',
-  lemonSqueezyApiKey: process.env.LEMON_SQUEEZY_API_KEY || '',
-  lemonSqueezySignSecret: process.env.LEMON_SQUEEZY_SIGN_SECRET || '',
-  sendGridApiKey: process.env.SENDGRID_API_KEY || '',
-  docStorageLocation: process.env.DOC_STORAGE_LOCATION || '../storage',
-  textractServiceUrl:
-    process.env.TEXTRACT_URL || 'http://localhost:8080/textract',
-  defaultSubscription:
-    Subscription[process.env.DEFAULT_SUBSCRIPTION] || Subscription.FREE,
-  senderEmail: process.env.EMAIL_SENDER_EMAIL || 'hi@webwhiz.ai',
-  senderName: process.env.EMAIL_SENDER_NAME || 'WebWhiz.ai',
-  clientUrl: process.env.CLIENT_URL || 'https://app.webwhiz.ai',
-  postgresHost: process.env.POSTGRES_HOST || '127.0.0.1',
-  postgresPort: parseInt(process.env.POSTGRES_PORT as string, 10) || 5432,
-  postgresUser: process.env.POSTGRES_USER || 'postgres',
-  postgresPassword: process.env.POSTGRES_PASSWORD || 'password',
-  postgresDbName: process.env.POSTGRES_DBNAME || 'sitemine',
-});
+const config = (): AppConfig => {
+  const featureFlags: FeatureFlags = {};
+
+  // Iterate over the environment variables and initialize feature flags
+  Object.keys(process.env).forEach((key) => {
+    if (key.startsWith('FF_')) {
+      const featureFlagName = key.replace('FF_', '').toLowerCase();
+      featureFlags[featureFlagName] = process.env[key] === 'true';
+    }
+  });
+
+  return {
+    nodeEnv: (process.env.NODE_ENV as any) || 'development',
+    host: process.env.HOST || '127.0.0.1',
+    port: parseInt(process.env.PORT as string, 10) || 8000,
+    secretKey: process.env.SECRET_KEY || 'secret',
+    encryptionKey:
+      process.env.ENC_KEY ||
+      '4b23f673b133b01777259525a4a1c2097fe309048db6593462312d5c35679ced',
+    mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017',
+    mongoDbName: process.env.MONGO_DBNAME || 'sitemine',
+    redisHost: process.env.REDIS_HOST || 'localhost',
+    redisPort: parseInt(process.env.REDIS_PORT as string, 10) || 6379,
+    redisUrl: process.env.REDIS_URL,
+    openaiKey: process.env.OPENAI_KEY || '',
+    openaiKey2: process.env.OPENAI_KEY_2 || '',
+    googleClientId: process.env.GOOGLE_CLIENT_ID || '',
+    lemonSqueezyApiKey: process.env.LEMON_SQUEEZY_API_KEY || '',
+    lemonSqueezySignSecret: process.env.LEMON_SQUEEZY_SIGN_SECRET || '',
+    sendGridApiKey: process.env.SENDGRID_API_KEY || '',
+    docStorageLocation: process.env.DOC_STORAGE_LOCATION || '../storage',
+    textractServiceUrl:
+      process.env.TEXTRACT_URL || 'http://localhost:8080/textract',
+    defaultSubscription:
+      Subscription[process.env.DEFAULT_SUBSCRIPTION] || Subscription.FREE,
+    senderEmail: process.env.EMAIL_SENDER_EMAIL || 'hi@webwhiz.ai',
+    senderName: process.env.EMAIL_SENDER_NAME || 'WebWhiz.ai',
+    clientUrl: process.env.CLIENT_URL || 'https://app.webwhiz.ai',
+    postgresHost: process.env.POSTGRES_HOST || '127.0.0.1',
+    postgresPort: parseInt(process.env.POSTGRES_PORT as string, 10) || 5432,
+    postgresUser: process.env.POSTGRES_USER || 'postgres',
+    postgresPassword: process.env.POSTGRES_PASSWORD || 'password',
+    postgresDbName: process.env.POSTGRES_DBNAME || 'sitemine',
+    featureFlags,
+  };
+};
 
 export { AppConfig };
 export default config;
